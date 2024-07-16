1129b64ef193764b6862e6702ac5925de7b16017
sergiorbotelho <sergiorbotelho@gmail.com>
2024-07-16 11:52:02 -0300

    feat: add header components

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,19 @@
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+import Header from "../components/header";
+import Constants from "expo-constants";
+
+const stausBarHeight = Constants.statusBarHeight;
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center">
-      <Text className="text-red-500 text-3xl">DevBotelho</Text>
-    </View>
+    <ScrollView
+      style={{ flex: 1 }}
+      className=" bg-slate-200"
+      showsVerticalScrollIndicator={false}
+    >
+      <View className="h-full px-4" style={{ marginTop: stausBarHeight + 8 }}>
+        <Header />
+      </View>
+    </ScrollView>
   );
 }
---
+++ src/components/header/index.tsx
@@ -0,0 +1,21 @@
+import { View, Text, ScrollView, Pressable } from "react-native";
+import { Ionicons, Feather } from "@expo/vector-icons";
+export default function Header() {
+  return (
+    <View className="flex flex-row justify-between">
+      <Pressable className="w-10 h-10 bg-white justify-center items-center rounded-full">
+        <Ionicons name="menu" size={20} color={"#121212"} />
+      </Pressable>
+      <View className="flex flex-col items-center justify-center">
+        <Text className="text-center text-sm text-slate-600">Localização</Text>
+        <View className="flex-row items-center justify-center gap-1">
+          <Feather name="map-pin" size={14} color={"#FF0000"} />
+          <Text className="text-lg font-bold">Recife</Text>
+        </View>
+      </View>
+      <Pressable className="w-10 h-10 bg-white justify-center items-center rounded-full">
+        <Feather name="bell" size={20} color={"#121212"} />
+      </Pressable>
+    </View>
+  );
+}
